591e02e68e37e836d0f5112485706e3cd0cc1510
Derek Eder <derek.eder@gmail.com>
2015-10-12 16:37:31 -0500

    setup makefile script, created geojson

--- data/config.mk
+++ data/config.mk
@@ -9,5 +9,10 @@ SHELL := /bin/bash
 # Variables specific to this build
 PG_HOST="localhost"
 PG_USER="derekeder"
-PG_DB="eitc"
+PG_DB="wv_eitc"
 PG_PORT="5432"
+
+HOUSE_DATA=wv_eitc_house.csv
+SENATE_DATA=wv_eitc_senate.csv
+HOUSE_SHAPE=cb_2014_54_sldl_500k
+SENATE_SHAPE=cb_2014_54_sldu_500k
