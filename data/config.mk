254f482869c740eafc150ae02dd29c090c46db7d
Eric van Zanten <eric.vanzanten@gmail.com>
2015-10-26 14:56:53 -0500

    Make sure Erikka Storch has correct county

--- data/config.mk
+++ data/config.mk
@@ -1,16 +1,16 @@
 # From http://clarkgrubb.com/makefile-style-guide#prologue
 MAKEFLAGS += --warn-undefined-variables
 SHELL := /bin/bash
-.SHELLFLAGS := -eu -o pipefail
+#.SHELLFLAGS := -eu -o pipefail
 .DEFAULT_GOAL := all
 .DELETE_ON_ERROR:
 .SUFFIXES:
 
 # Variables specific to this build
-PG_HOST="localhost"
-PG_USER="derekeder"
-PG_DB="wv_eitc"
-PG_PORT="5432"
+PG_HOST=localhost
+PG_USER=postgres
+PG_DB=wv_eitc
+PG_PORT=5432
 
 HOUSE_DATA=wv_eitc_house.csv
 SENATE_DATA=wv_eitc_senate.csv
