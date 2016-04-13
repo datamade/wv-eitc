4fc11c43694d1ec4a69d1f2a07c686515733605e
Derek Eder <derek.eder@gmail.com>
2016-04-13 14:50:39 -0500

    updated Rohrbach party, re-ran makefile

--- data/config.mk
+++ data/config.mk
@@ -1,14 +1,14 @@
 # From http://clarkgrubb.com/makefile-style-guide#prologue
 MAKEFLAGS += --warn-undefined-variables
 SHELL := /bin/bash
-#.SHELLFLAGS := -eu -o pipefail
+.SHELLFLAGS := -eu -o pipefail
 .DEFAULT_GOAL := all
 .DELETE_ON_ERROR:
 .SUFFIXES:
 
 # Variables specific to this build
 PG_HOST=localhost
-PG_USER=postgres
+PG_USER=derekeder
 PG_DB=wv_eitc
 PG_PORT=5432
 
